eb90219fc0f673a0e9597a1ffc9e95d879ddc9d8
Wolfgang Johannes Kohnen <wjkohnen@users.noreply.github.com>
2016-08-08 10:26:21 +0000

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# xzwriter: a trivial Go library for compressing with XZ.
+
+[![Apache License v2.0](https://img.shields.io/badge/license-Apache%20License%202.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0.txt)
+[![GoDoc](https://godoc.org/github.com/wjkohnen/xzwriter?status.svg)](https://godoc.org/github.com/wjkohnen/xzwriter)
+
+Package xzwriter provides a writer XZWriter that pipes through an external XZ
+compressor.
+
+Expects the Tukaani XZ tool in $PATH. See the XZ Utils home page:
+<http://tukaani.org/xz/>
+
+As this is a trivial convenience package, you should probably not import this. Anyway
+licensed under the Apache License, Version 2.0.
---
+++ xzwriter.go
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2016 Wolfgang Johannes Kohnen <wjkohnen@users.noreply.github.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Package xzwriter provides a writer XZWriter that pipes through an external XZ
+// compressor.
+//
+// Expects the Tukaani XZ tool in $PATH. See the XZ Utils home page:
+// <http://tukaani.org/xz/>
+package xzwriter
+
+import (
+	"context"
+	"io"
+	"os/exec"
+)
+
+// XZWriter is a writer that wraps a writer around an XZ compressor.
+type XZWriter struct {
+	cmd  *exec.Cmd
+	pipe io.WriteCloser
+}
+
+// New returns an XZWriter, wrapping another writer.
+func New(w io.Writer) (xzwriter *XZWriter, err error) {
+	return NewWithContext(context.Background(), w)
+}
+
+// NewWithContext returns an XZWriter, wrapping another writer. The Context may
+// be used to cancel or timeout the external compressor process.
+//
+// The context can be used to kill the external process early. You still need to
+// call Close() to clean up ressources. Alternatively you may call Close()
+// prematurely.
+func NewWithContext(ctx context.Context, w io.Writer) (*XZWriter, error) {
+	xz := &XZWriter{}
+	var err error
+
+	if ctx == nil {
+		ctx = context.Background()
+	}
+
+	xz.cmd = exec.CommandContext(ctx, "xz", "--quiet", "--compress",
+		"--stdout", "--best", "-")
+	xz.cmd.Stdout = w
+	xz.pipe, err = xz.cmd.StdinPipe()
+	if err != nil {
+		return nil, err
+	}
+
+	err = xz.cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+
+	return xz, err
+}
+
+// Write implements the io.Writer interface.
+func (xz *XZWriter) Write(p []byte) (n int, err error) {
+	return xz.pipe.Write(p)
+}
+
+// Close implements the io.Writer interface.
+func (xz *XZWriter) Close() error {
+	errPipe := xz.pipe.Close()
+	errWait := xz.cmd.Wait()
+	if errPipe != nil {
+		return errPipe
+	}
+	return errWait
+}
